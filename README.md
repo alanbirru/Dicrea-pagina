fdd4cb64c113721134bfef28ed9ff5351b8eedd8
alanbirru <alanb_99@hotmail.com>
2023-11-14 15:36:06 -0700

    adding read me file and making the serviciosArr work

---
+++ README.md
@@ -0,0 +1,32 @@
+# Pagina web de dicrea
+
+- Página web para la empresa de marketing digital.
+
+# Uso
+
+## iniciar proyecto
+
+- iniciar con: "npm run start"
+
+## actualizar el proyecto
+
+los siguientes comandos son para actualizar cambios a github
+
+1. "git add ."
+2. "git commit -m "añadir descripcion del cambio" "
+3. "git push"
+4. "npm run build"
+5. "npm run deploy"
+
+# paquetes npm
+
+los siguientes paquetes son los que se utilizaron en el proyecto, con una breve descripción.
+
+1. aos:
+2. lottie-web:
+3. react-animated-cursor:
+4. react-parallax-tilt:
+5. react-tsparticles:
+6. react-type-animation:
+
+# Estructura del Proyecto
--- src/components/sections/Servicios/Servicios.js
+++ src/components/sections/Servicios/Servicios.js
@@ -1,93 +1,17 @@
 import { useState } from "react";
 
+// --- START SERVICIOS ARRAY---
+import serviciosArr from "./serviciosArr";
+// --- END SERVICIOS ARRAY---
+
 // START ----STYLES----
 import "../../../styles/servicios.css";
 // END ----STYLES----
 
-// START ----COMPONENTS----
 import ServiciosTitle from "./ServiciosTitle";
 import ServiciosSection from "./ServiciosSection";
-// END ----COMPONENTS----
-
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
-// START ---- IMPORT-ICONS-SERVISES ----
-import { faBullhorn } from "@fortawesome/free-solid-svg-icons";
-import { faComputer } from "@fortawesome/free-solid-svg-icons";
-import { faCamera } from "@fortawesome/free-solid-svg-icons";
-// END ---- IMPORT-ICONS-SERVISES ----
-
-// START ----IMAGES----
-import marketingImage from "../../../images/marketing.webp";
-import studioImage from "../../../images/studio.webp";
-import webServicesImage from "../../../images/webservices.webp";
-// END ----IMAGES----
 
 const Servicios = ({ id }) => {
-  // START ----SERVICIOS-ICON-VARIABLES----
-  const marketingIcon = (
-    <FontAwesomeIcon className="marketingIcon" icon={faBullhorn} />
-  );
-  const webservicesIcon = (
-    <FontAwesomeIcon className="webservicesIcon" icon={faComputer} />
-  );
-  const studioIcon = <FontAwesomeIcon className="studioIcon" icon={faCamera} />;
-  // END ----SERVICIOS-ICON-VARIABLES----
-
-  // START ----ARRAY-OF-SECTIONS----
-  const sections = [
-    {
-      title: "Marketing",
-      description:
-        "Posicionamos tu marca haciendo que estés presente cuando tus clientes necesitan justo lo que tu marca ofrece.",
-      id: "marketing",
-      icon: marketingIcon,
-      image: marketingImage,
-      services: [
-        "Manejo de redes sociales",
-        "Manejo de reputación",
-        "Instagram Ads",
-        "Email marketing",
-        "Facebook Ads",
-        "Tiktok Ads",
-      ],
-    },
-    {
-      title: "Web Services",
-      description:
-        "Con sitios web totalmente responsivos, limpios y funcionales que se convierten en tu mejor vendedor 24/7.",
-      id: "webservices",
-      icon: webservicesIcon,
-      image: webServicesImage,
-      services: [
-        "Páginas web",
-        "Optimización web",
-        "Posicionamiento SEO/SEM",
-        "Correos profesionales",
-        "e-Commerce",
-      ],
-    },
-    {
-      title: "Studio",
-      description:
-        "Logra llegar a tu público objetivo con estrategías de verdadero impacto aplicado en diseños, videos, animacion, flyers con realidad aumentada, dale vida propia a tu logotipo y mucho más",
-      id: "studio",
-      icon: studioIcon,
-      image: studioImage,
-      services: [
-        "Sesión Fotográfica",
-        "Diseño de identidad",
-        "Animación",
-        "Diseño para imprenta",
-        "Sesión de video",
-        "Diseño para redes sociales",
-        "Realidad aumentada",
-      ],
-    },
-  ];
-  // END ----ARRAY-OF-SECTIONS----
-
-  // START ----ACTIVE-SECTION----
   const [activeSection, setActiveSection] = useState("marketing");
 
   const handleSectionClick = (section) => {
@@ -97,7 +21,7 @@ const Servicios = ({ id }) => {
   // END ----ACTIVE-SECTION----
 
   // START ----DISPLAY-TITLES----
-  const displayServiciosTitle = sections.map((item) => (
+  const displayServiciosTitle = serviciosArr.map((item) => (
     <ServiciosTitle
       title={item.title}
       id={item.id}
@@ -117,7 +41,7 @@ const Servicios = ({ id }) => {
       <h2>Nuestros Servicios</h2>
 
       <div className="servicios-title">{displayServiciosTitle}</div>
-      <ServiciosSection sections={sections} activeSection={activeSection} />
+      <ServiciosSection sections={serviciosArr} activeSection={activeSection} />
     </div>
   );
 };
